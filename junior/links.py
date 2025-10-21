1655fdf552ff77d7c05711ce59b2adc4a61b8310
Sandroisu <sandroisu1411@gmail.com>
2025-10-21 23:54:08 +0300

    minor refactoring

--- junior/links.py
+++ junior/links.py
@@ -18,11 +18,15 @@ b = [0]
 reassign_list(b)
 print(b)  # всё ещё [0]
 
+
 def mutate(a_list):
     a_list += [4, 5]
 
+
 def reassign(a_list):
     a_list = a_list + [6, 7]
+    print(a_list)
+
 
 x = [1, 2, 3]
 mutate(x)
