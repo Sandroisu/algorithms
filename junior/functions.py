0780d53fd410cdf5b95eeb55a86f59fbf8c77f5b
Sandroisu <sandroisu1411@gmail.com>
2025-10-27 21:30:21 +0300

    continuing with functions

--- junior/functions.py
+++ junior/functions.py
@@ -4,3 +4,20 @@ def greet(name):
 say_hi = greet   # ← просто присваиваем функцию переменной
 print(say_hi("Alice"))
 
+def apply_twice(func, x):
+    return func(func(x))
+
+def add_five(n):
+    return n + 5
+
+result = apply_twice(add_five, 10)
+print(result)
+
+
+def outer():
+    x = 10
+    def inner():
+        print(f"x = {x}")
+    inner()
+
+outer()
