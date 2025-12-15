8ba00cfb8e93af89758570c1d4163a227c54b091
Sandroisu <sandroisu1411@gmail.com>
2025-12-15 22:22:28 +0300

    added is_prime

--- junior/odd.py
+++ junior/odd.py
@@ -250,3 +250,14 @@ def longest_unique_substring(s):
 
     return best
 
+def is_prime(n):
+    if n < 2:
+        return False
+
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+
+    return True
