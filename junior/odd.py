07bf61015f9725bc9ce1654cf7570acefefe890e
Sandroisu <sandroisu1411@gmail.com>
2025-12-16 23:02:23 +0300

    added is valid brackets

--- junior/odd.py
+++ junior/odd.py
@@ -261,3 +261,20 @@ def is_prime(n):
         i += 1
 
     return True
+
+def is_valid_brackets(s):
+    stack = []
+    pairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for ch in s:
+        if ch in pairs.values():
+            stack.append(ch)
+        elif ch in pairs:
+            if not stack or stack.pop() != pairs[ch]:
+                return False
+
+    return len(stack) == 0
