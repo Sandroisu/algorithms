01b06a1fdce9bc79308f4f040ebe682b1cac03b8
Sandroisu <sandroisu1411@gmail.com>
2025-12-18 22:10:02 +0300

    added binary search

--- junior/odd.py
+++ junior/odd.py
@@ -278,3 +278,20 @@ def is_valid_brackets(s):
                 return False
 
     return len(stack) == 0
+
+
+def binary_search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        if nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
